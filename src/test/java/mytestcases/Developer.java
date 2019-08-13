5da16233b8491b501b247afc706a6ff4fb9a4ba3
Kunal7008 <kunal.bhatta513@gmail.com>
2019-08-13 10:58:54 +0530

    latest development

---
+++ src/test/java/mytestcases/Developer.java
@@ -0,0 +1,10 @@
+package mytestcases;
+
+import org.testng.annotations.Test;
+
+public class Developer {
+    @Test
+    public void get(){
+        System.out.println("writing some code for java");
+    }
+}
